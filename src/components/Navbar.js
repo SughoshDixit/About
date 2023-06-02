77be03dcd03dbccbb7b34b1b0ada4b2244c4f1af
SughoshDixit <sughoshpdixit@gmail.com>
2023-06-02 08:46:59 +0530

    page redirect fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Navbar.Brand href="https://sughoshdixit.github.io/About/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
